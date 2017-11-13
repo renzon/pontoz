ed7cdf81a9f50ac25e2cbb17629a02194b66ee94
renzon <renzon@gmail.com>
2017-11-13 11:13:53 -0200

    Added DB schema

--- db/tables.py
+++ db/tables.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine, DateTime, DECIMAL
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
 
@@ -10,6 +10,33 @@ class Partner(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(60))
     segment = Column(String(60))
+    # regions = relationship('Region')
+
+
+class Region(Base):
+    __tablename__ = 'region'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(60))
+    partner_id = Column(Integer, ForeignKey('partner.id'))
+    partner = relationship(Partner)
+
+
+class Store(Base):
+    __tablename__ = 'store'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(60))
+    region_id = Column(Integer, ForeignKey('region.id'))
+    region = relationship(Region)
+
+
+class Transaction(Base):
+    __tablename__ = 'store'
+    id = Column(Integer, primary_key=True)
+    store_id = Column(Integer, ForeignKey('store.id'))
+    store = relationship(Store)
+    creation = Column(DateTime())
+    sale = Column(DECIMAL(2))
+    ponitz_sale = Column(DECIMAL(2))
 
 
 engine = create_engine(
@@ -19,5 +46,3 @@ engine = create_engine(
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
-
-
