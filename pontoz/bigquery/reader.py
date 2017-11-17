1ff6c78c2acd6680de589066d63a6ba47adf193c
renzon <renzon@gmail.com>
2017-11-17 16:48:35 -0200

    Added function to get max transaction id on BigQuery

--- pontoz/bigquery/reader.py
+++ pontoz/bigquery/reader.py
@@ -46,6 +46,24 @@ def get_annual_dre_by_client_region(year=None):
     return query_job.result()
 
 
+MAX_TRANSACTION_ID_QUERY = """
+SELECT
+  MAX(id) as max_id
+FROM
+  `pontoz.transactions`
+"""
+
+
+def get_max_transaction_id():
+    job_config = bigquery.QueryJobConfig()
+    query_job = client.query(MAX_TRANSACTION_ID_QUERY, job_config=job_config)  # API request - starts the query
+
+    # Waits for the query to finish
+    row = list(query_job.result())[0]
+    return row.max_id if row.max_id is not None else 0
+
+
 if __name__ == '__main__':
-    annual_reports = get_annual_dre_by_client_region()
-    print(list(annual_reports))
+    # annual_reports = get_annual_dre_by_client_region()
+    # print(list(annual_reports))
+    print(get_max_transaction_id())
